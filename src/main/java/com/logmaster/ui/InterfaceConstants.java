52db4117443275fe7e03edade10f69042b1e4308
Raphael Mobis Tacla <r.mobis@gmail.com>
2025-08-13 02:54:24 -0300

    remove unused sprites

--- src/main/java/com/logmaster/ui/InterfaceConstants.java
+++ src/main/java/com/logmaster/ui/InterfaceConstants.java
@@ -5,19 +5,15 @@ public class InterfaceConstants {
     public static final String DEF_FILE_SPRITES = "SpriteDef.json";
     public static final int TRANSPARENT_SPRITE_ID = -20099;
 
-    public static final int TASK_BACKGROUND_SPRITE_ID = -20006;
     public static final int TASK_LIST_BACKGROUND_SPRITE_ID = -20012;
     public static final int TASK_COMPLETE_BACKGROUND_SPRITE_ID = -20013;
     public static final int TASK_CURRENT_BACKGROUND_SPRITE_ID = -20016;
 
     public static final int COLLECTION_LOG_WINDOW_WIDTH = 500;
     public static final int COLLECTION_LOG_WINDOW_HEIGHT = 314;
-    public static final int COLLECTION_LOG_CONTENT_WIDGET_ID = 40697858;
 
     public static final int DASHBOARD_TAB_SPRITE_ID = -20007;
     public static final int DASHBOARD_TAB_HOVER_SPRITE_ID = -20008;
-    public static final int TASKLIST_TAB_SPRITE_ID = -20009;
-    public static final int TASKLIST_TAB_HOVER_SPRITE_ID = -20010;
     public static final int DIVIDER_SPRITE_ID = -20011;
 
     public static final int TASKLIST_EASY_TAB_SPRITE_ID = -20017;
