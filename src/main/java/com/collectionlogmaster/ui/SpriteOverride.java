7d0d3d1915a4c58ba134d8e4f3dc1ec822f6b7af
Raphael Mobis Tacla <r.mobis@gmail.com>
2025-08-29 03:31:13 -0300

    auto decide sprite override ids

--- src/main/java/com/collectionlogmaster/ui/SpriteOverride.java
+++ src/main/java/com/collectionlogmaster/ui/SpriteOverride.java
@@ -1,36 +1,47 @@
 package com.collectionlogmaster.ui;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public enum SpriteOverride implements net.runelite.client.game.SpriteOverride {
-    CURRENT_TASK(-20006, "currentTask.png"),
-    TAB_DASHBOARD(-20007, "tab-dashboard.png"),
-    TAB_DASHBOARD_HOVER(-20008, "tab-dashboard-hover.png"),
-    DIVIDER(-20011, "divider.png"),
-    TASK_LONG(-20012, "task-long.png"),
-    TASK_LONG_COMPLETE(-20013, "task-long-complete.png"),
-    UP_ARROW(-20014, "up-arrow.png"),
-    DOWN_ARROW(-20015, "down-arrow.png"),
-    TASK_LONG_CURRENT(-20016, "task-long-current.png"),
-    EASY_TAB(-20017, "easy-tab.png"),
-    EASY_TAB_HOVER(-20018, "easy-tab-hover.png"),
-    MEDIUM_TAB(-20019, "medium-tab.png"),
-    MEDIUM_TAB_HOVER(-20020, "medium-tab-hover.png"),
-    HARD_TAB(-20021, "hard-tab.png"),
-    HARD_TAB_HOVER(-20022, "hard-tab-hover.png"),
-    ELITE_TAB(-20023, "elite-tab.png"),
-    ELITE_TAB_HOVER(-20024, "elite-tab-hover.png"),
-    MASTER_TAB(-20025, "master-tab.png"),
-    MASTER_TAB_HOVER(-20026, "master-tab-hover.png"),
-    PAGE_UP_ARROW(-20029, "page-up-arrow.png"),
-    PAGE_DOWN_ARROW(-20030, "page-down-arrow.png"),
-    THUMB_TOP(-20031, "thumb-top.png"),
-    THUMB_MIDDLE(-20032, "thumb-middle.png"),
-    THUMB_BOTTOM(-20033, "thumb-bottom.png"),
-    TRANSPARENT(-20099, "transparent.png");
+    CURRENT_TASK("currentTask.png"),
+    TAB_DASHBOARD("tab-dashboard.png"),
+    TAB_DASHBOARD_HOVER("tab-dashboard-hover.png"),
+    DIVIDER("divider.png"),
+    TASK_LONG("task-long.png"),
+    TASK_LONG_COMPLETE("task-long-complete.png"),
+    UP_ARROW("up-arrow.png"),
+    DOWN_ARROW("down-arrow.png"),
+    TASK_LONG_CURRENT("task-long-current.png"),
+    EASY_TAB("easy-tab.png"),
+    EASY_TAB_HOVER("easy-tab-hover.png"),
+    MEDIUM_TAB("medium-tab.png"),
+    MEDIUM_TAB_HOVER("medium-tab-hover.png"),
+    HARD_TAB("hard-tab.png"),
+    HARD_TAB_HOVER("hard-tab-hover.png"),
+    ELITE_TAB("elite-tab.png"),
+    ELITE_TAB_HOVER("elite-tab-hover.png"),
+    MASTER_TAB("master-tab.png"),
+    MASTER_TAB_HOVER("master-tab-hover.png"),
+    PAGE_UP_ARROW("page-up-arrow.png"),
+    PAGE_DOWN_ARROW("page-down-arrow.png"),
+    THUMB_TOP("thumb-top.png"),
+    THUMB_MIDDLE("thumb-middle.png"),
+    THUMB_BOTTOM("thumb-bottom.png"),
+    TRANSPARENT("transparent.png");
+
+    // we put `lastSpriteId` into a nested static class to force the
+    // JVM into initializing it before calling the enum constructor
+    private static class Memory {
+        private static int lastSpriteId = -20000;
+    }
+
+    SpriteOverride(String fileName) {
+        // we don't really care what the ID is, as long as it's
+        // not repeated and we can reference it from the enum
+        this.spriteId = --Memory.lastSpriteId;
+        this.fileName = fileName;
+    }
 
     private final int spriteId;
 
