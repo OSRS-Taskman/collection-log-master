38e4c99d7165758da29a18a70d0aa73f5254b40e
Raphael Mobis Tacla <r.mobis@gmail.com>
2025-08-29 04:54:10 -0300

    add vflipped tab edge sprites

--- src/main/java/com/collectionlogmaster/util/EventBusSubscriber.java
+++ src/main/java/com/collectionlogmaster/util/EventBusSubscriber.java
@@ -6,7 +6,7 @@ import javax.inject.Inject;
 
 public abstract class EventBusSubscriber {
     @Inject
-    EventBus eventBus = null;
+	protected EventBus eventBus = null;
 
     public void startUp() {
         eventBus.register(this);
