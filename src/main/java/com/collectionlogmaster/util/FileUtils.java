06f792444c6244a91dfd1ffb5d4812c2b7e0ea7f
Raphael Mobis Tacla <r.mobis@gmail.com>
2025-08-29 03:31:39 -0300

    get rid of loadDefinitionResource

--- src/main/java/com/collectionlogmaster/util/FileUtils.java
+++ src/main/java/com/collectionlogmaster/util/FileUtils.java
@@ -10,24 +10,6 @@ import java.lang.reflect.Type;
 import static com.collectionlogmaster.util.GsonOverride.GSON;
 
 public class FileUtils {
-    /**
-     * Loads a definition resource from a JSON file
-     *
-     * @param classType the class into which the data contained in the JSON file will be read into
-     * @param resource  the name of the resource (file name)
-     * @param <T>       the class type
-     * @return the data read from the JSON definition file
-     */
-    public static <T> T loadDefinitionResource(Class<T> classType, String resource) {
-        // Load the resource as a stream and wrap it in a reader
-        InputStream resourceStream = classType.getResourceAsStream(resource);
-        assert resourceStream != null;
-        InputStreamReader definitionReader = new InputStreamReader(resourceStream);
-
-        // Load the objects from the JSON file
-        return GSON.fromJson(definitionReader, classType);
-    }
-
     public static <T> T loadResource(String resourcePath, Type clazz) {
         try (InputStream is = CollectionLogMasterPlugin.class.getResourceAsStream(resourcePath)) {
             assert is != null;
