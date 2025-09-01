7b7e794479743733080249e173ceba07faef64a8
Raphael Mobis Tacla <r.mobis@gmail.com>
2025-09-01 02:19:10 -0300

    remove all unused sprites

--- src/main/java/com/collectionlogmaster/domain/TaskTier.java
+++ src/main/java/com/collectionlogmaster/domain/TaskTier.java
@@ -1,21 +1,16 @@
 package com.collectionlogmaster.domain;
 
-import com.collectionlogmaster.ui.SpriteOverride;
 
 public enum TaskTier {
-	EASY("Easy", SpriteOverride.EASY_TAB.getSpriteId(), SpriteOverride.EASY_TAB_HOVER.getSpriteId()),
-	MEDIUM("Medium", SpriteOverride.MEDIUM_TAB.getSpriteId(), SpriteOverride.MEDIUM_TAB_HOVER.getSpriteId()),
-	HARD("Hard", SpriteOverride.HARD_TAB.getSpriteId(), SpriteOverride.HARD_TAB_HOVER.getSpriteId()),
-	ELITE("Elite", SpriteOverride.ELITE_TAB.getSpriteId(), SpriteOverride.ELITE_TAB_HOVER.getSpriteId()),
-	MASTER("Master", SpriteOverride.MASTER_TAB.getSpriteId(), SpriteOverride.MASTER_TAB_HOVER.getSpriteId());
+	EASY("Easy"),
+	MEDIUM("Medium"),
+	HARD("Hard"),
+	ELITE("Elite"),
+	MASTER("Master");
 
 	public final String displayName;
-	public final int tabSpriteId;
-	public final int tabSpriteHoverId;
 
-	TaskTier(String displayName, int tabSpriteId, int tabSpriteHoverId) {
+	TaskTier(String displayName) {
 		this.displayName = displayName;
-		this.tabSpriteId = tabSpriteId;
-		this.tabSpriteHoverId = tabSpriteHoverId;
 	}
 }
