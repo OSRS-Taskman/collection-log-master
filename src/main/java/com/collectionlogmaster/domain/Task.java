d302088d41604d65d071f68ac9eb913cd6394d73
Raphael Mobis Tacla <r.mobis@gmail.com>
2025-12-17 04:06:49 -0300

    make code spotless

--- src/main/java/com/collectionlogmaster/domain/Task.java
+++ src/main/java/com/collectionlogmaster/domain/Task.java
@@ -1,32 +1,31 @@
 package com.collectionlogmaster.domain;
 
 import com.collectionlogmaster.domain.verification.Verification;
-import lombok.Data;
-
-import javax.annotation.Nullable;
 import java.util.HashSet;
 import java.util.Set;
+import javax.annotation.Nullable;
+import lombok.Data;
 
 @Data
 public class Task {
-    private String id;
-    private String name;
-    private String tip;
-    private String wikiLink;
-    private int displayItemId;
-    private Set<Tag> tags;
+	private String id;
+	private String name;
+	private String tip;
+	private String wikiLink;
+	private int displayItemId;
+	private Set<Tag> tags;
 
-    private @Nullable Verification verification;
+	private @Nullable Verification verification;
 
-    public Set<Tag> getTags() {
-        if (tags == null) {
-            tags = new HashSet<>();
-        }
+	public Set<Tag> getTags() {
+		if (tags == null) {
+			tags = new HashSet<>();
+		}
 
-        return tags;
-    }
+		return tags;
+	}
 
-    public boolean isLMS() {
-        return getTags().contains(Tag.LMS);
-    }
+	public boolean isLMS() {
+		return getTags().contains(Tag.LMS);
+	}
 }
