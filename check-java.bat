570600bafb05a3c58753807205cd0a1503cec966
Raphael Mobis Tacla <r.mobis@gmail.com>
2026-05-26 23:22:47 -0300

    update check-java.bat to output java version

--- check-java.bat
+++ check-java.bat
@@ -1,7 +1,10 @@
 @echo off
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if %ERRORLEVEL% equ 0 echo Java found!
+set JAVA_EXE=javad.exe
+
+%JAVA_EXE% -version
+echo.
+if not %ERRORLEVEL% equ 0 echo Java NOT found!
+if not %ERRORLEVEL% equ 0 echo Java NOT found!
 if not %ERRORLEVEL% equ 0 echo Java NOT found!
 pause
